0da7d11de946b5ca048a53cf9cbcd96fd9aed048
Muhammad Farooq <farooqhmf@hotmail.com>
2022-04-02 05:53:01 +0500

    footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,8 +4,11 @@ import Image from "next/image";
 const Footer = () => {
     return(
         <div className="footer">
+            <div className="footer-nft-Icons">
+                <Image width={1200} height={300} layout='responsive' src="/images/NFT-Icons.png" alt="border footer"/>
+            </div>
             <div className="footer-image">
-                {/*<Image width={1500} height={1} layout='fixed' objectFit="cover" src="/images/sidebar-border.png" alt="border footer"/>*/}
+                <Image width={1200} height={1} layout='responsive' src="/images/sidebar-border.png" alt="border footer"/>
             </div>
             <div className="footer-content">
                 <div className="rights-Reserved">
