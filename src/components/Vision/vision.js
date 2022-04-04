3d9423709be02bc5c1f2f97bda3635947adbe037
Farooq <you@example.com>
2022-04-04 23:01:30 +0500

    working on screens

--- src/components/Vision/vision.js
+++ src/components/Vision/vision.js
@@ -13,11 +13,11 @@ const vision = () => {
             <Image src='/ring.png' width={40} height={60} alt='ring' /></span>
             <span className='text-green'>N</span>
         </h1>
-        <div className='vision-setter'>
-          <h1 className='vision-h1 mobile-vision' data-aos-once="true" data-aos-delay="100" data-aos-duration="2000" data-aos="fade-up">
+        <div className='vision-setter' data-aos-once="true" data-aos-delay="100" data-aos-duration="2000" data-aos="fade-up">
+          <h1 className='vision-h1 mobile-vision' >
           THE <span className='text-green'>visi</span><span className='opacity-o'>o</span><span className='text-green'>n</span>
         </h1>
-        <div className='mobile-ring'><Image src='/ring.png' width={28} height={43} alt='ring' /></div>
+        <div className='mobile-ring' ><Image src='/ring.png' width={28} height={43} alt='ring' /></div>
         </div>
         <p className='vision-p' data-aos-once="true" data-aos-delay="200" data-aos-duration="2000" data-aos="fade-up">
             Our vision for the Sneaky Goblins is to be the Genesis collection of a larger upcoming ecosystem. Over time, we are building the components of an NFT & Metaverse Studio to empower our community.
