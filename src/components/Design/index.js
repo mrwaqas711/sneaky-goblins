8d0f2bd31b65433edf66e5f087410f6e3cbff357
Muhammad Farooq <farooqhmf@hotmail.com>
2022-04-05 02:58:05 +0500

    mnior change

--- src/components/Design/index.js
+++ src/components/Design/index.js
@@ -44,7 +44,7 @@ const properties = {
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 5,
+                slidesToShow: 4,
                 slidesToScroll: 1
             }
         },
