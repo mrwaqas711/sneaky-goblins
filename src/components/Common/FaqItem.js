7be889827d1926c659edb9424d214784b2f19f75
Farooq <you@example.com>
2022-04-07 01:56:24 +0500

    faq fixes

--- src/components/Common/FaqItem.js
+++ src/components/Common/FaqItem.js
@@ -1,16 +1,17 @@
 import React, { useState} from 'react';
+import Collapsible from 'react-collapsible';
 
 const FaqItem = ({faq}) => {
     const [isOpen, setIsOpen] = useState(false);
+    let element = <div className='faq-text' onClick={() => setIsOpen(!isOpen)}>
+                    <div>{faq.q}</div>
+                    <div className='plus-icon'><div className={`plus-animated ${isOpen ? 'animated-active' : 'animated-inactive'}`}></div></div>
+                </div>
     return (
-        <div className="faq" >
-            <div className='faq-text' onClick={() => setIsOpen(!isOpen)}>
-                <div>{faq.q}</div>
-                <div className='plus-icon'>+</div>
-            </div>
-            {
-                isOpen && <div className='collap-text'>{faq.a}</div>
-            }
+        <div className="faq" >       
+            <Collapsible trigger={element}>
+                <div className='collap-text'>{faq.a}</div>
+            </Collapsible>
         </div>
     );
 }
